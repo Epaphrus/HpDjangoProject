c69b146cdf116cb4260730f6e82b7b8d10442917
Maxwel .E. Khadambi <83645259+Epaphrus@users.noreply.github.com>
2023-10-26 22:40:59 +0300

    added gallery.html to the urls path

--- HpDjangoProject/urls.py
+++ HpDjangoProject/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='homepage'),
     path('about/', views.about, name='about'),
+    path('gallery/', views.gallery, name='gallery')
 ]
