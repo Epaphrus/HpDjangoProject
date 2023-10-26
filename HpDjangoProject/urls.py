25f4c56cc7f6b33238c8aff5222d9b332bb50228
Maxwel .E. Khadambi <83645259+Epaphrus@users.noreply.github.com>
2023-10-26 08:33:38 +0300

    django

--- HpDjangoProject/urls.py
+++ HpDjangoProject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from HpDjangoProject import views
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Home, name='homepage')
 ]
